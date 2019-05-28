526dd0052f98cb6ffb7210a2e304a4738a420ca7
Rob Johnson <rob.johnson@myunidays.com>
2019-05-28 09:56:53 +0100

    OG Fizzbuzz

---
+++ fizzbuzzapp1/fizzbuzzapp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace fizzbuzzapp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			for (int i = 1; i <= 100; i++)
+			{
+				 if (i % 3 == 0 && i % 5 == 0)
+				{
+					Console.WriteLine("FizzBuzz!");
+				}
+
+				else if (i % 3 == 0)
+				{
+					Console.WriteLine("Fizz");
+				}
+				else if (i % 5 == 0)
+				{
+					Console.WriteLine("Buzz");
+
+				}
+				
+				else { Console.WriteLine(i); }
+			}
+
+			/*
+			 * using System;
+
+namespace fizzbuzzapp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			for (int i = 1; i <= 100; i++)
+			{
+				var output = "";
+				if (i % 3 == 0)
+					output = "fizz";
+
+				if (i % 5 == 0)
+					output += "buzz";
+
+				if (string.IsNullOrEmpty(output))
+					output = i.ToString();
+
+				Console.WriteLine(output);
+			}
+
+
+
+			Console.Read();
+		}
+	}
+}
+*/
+			
+
+			Console.Read();
+		}
+	}
+}
