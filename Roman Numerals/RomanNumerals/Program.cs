ef42c5488c42454e8803e1f9de47fe826d643d08
Rob Johnson <rob.johnson@myunidays.com>
2019-05-30 09:01:13 +0100

    working array

--- Roman Numerals/RomanNumerals/Program.cs
+++ Roman Numerals/RomanNumerals/Program.cs
@@ -2,50 +2,41 @@
 
 namespace RomanNumerals
 {
-	class Program
+	class romans
+	
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Who  is the Duck?");
-			for (int i = 1; i <= 60; i++)
+			Console.WriteLine("Roman Numerals");
+
+			int[] intArray1 = new int[7];
+			intArray1[0] = 1;
+			intArray1[1] = 5;
+			intArray1[2] = 10;
+			intArray1[3] = 50;
+			intArray1[4] = 100;
+			intArray1[5] = 500;
+			intArray1[6] = 1000;
+
+			int x = 0;
+
+			while (x <= 3)
 			{
-				if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0)
-				{
-					Console.WriteLine("DUCKTALES!");
-				}
-
-				else if (i % 2 == 0 && i % 3 == 0)
-				{
-					Console.WriteLine("HueyLouey!");
-				}
-
-				else if (i % 2 == 0 && i % 5 == 0)
-				{
-					Console.WriteLine("HueyDouey!");
-				}
-
-				else if (i % 3 == 0 && i % 5 == 0)
-				{
-					Console.WriteLine("LoueyDouey!");
-				}
-
-				else if (i % 2 == 0)
-				{
-					Console.WriteLine("Huey");
-				}
-
-				else if (i % 3 == 0)
-				{
-					Console.WriteLine("Louey");
-				}
-				else if (i % 5 == 0)
-				{
-					Console.WriteLine("Douey");
-
-				}
-
-				else { Console.WriteLine(i); }
+				for (int i = 0; i < intArray1.Length; i++)
+					Console.WriteLine(intArray1[i]);
+				x++;
 			}
+
+			//for(int i =0; i < intArray1.Length; i++)
+			//	Console.WriteLine(intArray1[i]);
+		
+			//Console.WriteLine(intArray1[0]);
+			//Console.WriteLine(intArray1[1]);
+			//Console.WriteLine(intArray1[2]);
+			//Console.WriteLine(intArray1[3]);
+			//Console.WriteLine(intArray1[4]);
+			//Console.WriteLine(intArray1[5]);
+			//Console.WriteLine(intArray1[6]);
 
 			Console.Read();
 		}
