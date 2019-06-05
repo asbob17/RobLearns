1321619f471baa257cdbb1dd2b9822d3d80c79fa
Rob Johnson <rob.johnson@myunidays.com>
2019-06-05 14:03:08 +0100

    crude working version

--- Roman Numerals/RomanNumerals/Roman.cs
+++ Roman Numerals/RomanNumerals/Roman.cs
@@ -15,13 +15,79 @@ namespace RomanNumerals
 
 			int x = 1;
 
-				while (x > 0)
+			while (x >= 0)
 			{
-				string input;
-				Console.WriteLine("Enter a Roman Numeral to convert - ");
-				input = Console.ReadLine();
+				int input;
+				Console.WriteLine("Enter a Number to convert - ");
+				input = int.Parse(Console.ReadLine());
 				Console.WriteLine("You entered - '{0}'", input);
 				x--;
+
+				var output = string.Empty;
+
+				while (input > 0)
+				{
+					if (input >= 1000)
+					{
+						output += "M";
+						input -= 1000;
+						continue;
+					}
+
+					if (input >= 500)
+					{
+						output += "D";
+						input -= 500;
+						continue;
+					}
+
+					if (input >= 100)
+					{
+						output += "C";
+						input -= 100;
+						continue;
+					}
+
+					if (input >= 50)
+					{
+						output += "L";
+						input -= 50;
+						continue;
+					}
+
+					if (input >= 10)
+					{
+						output += "X";
+						input -= 10;
+						continue;
+					}
+
+					if (input >= 9)
+					{
+						output += "IX";
+						input -= 9;
+						continue;
+					}
+
+					if (input >= 5)
+					{
+						output += "V";
+						input -= 5;
+						continue;
+					}
+
+					if (input >= 4)
+					{
+						output += "IV";
+						input -= 4;
+						continue;
+					}
+
+					output += "I";
+					input -= 1;
+				}
+
+				Console.WriteLine(output);
 			}
 
 			Console.Read();
