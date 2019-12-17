996364517fa57747c3124d0bc5bac2b67fec7a8b
Rob Johnson <rob.johnson@myunidays.com>
2019-12-17 14:22:28 +0000

    added files. heelo world

---
+++ tutorials/tutorial1/tutorial1/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            //a simple hello world application
+            Console.WriteLine("Hello you cunts");
+            Console.ReadKey();
+        }
+    }
+}
